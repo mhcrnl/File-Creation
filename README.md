487ee3f109f2a6a67560f7f08ba29c0a2b29b2c3
Mihai Cornel <mhcrnl@gmail.com>
2022-07-10 09:10:21 +0300

    10.07.2022

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Folder-Creation
+# Folder and Files Creation for Python Apps
+This app crate main.py file and README.md file inside the folder you created.
+
+## Installation
+```
+git clone https://github.com/mhcrnl/File-Creation.git
+```
+
+## Usage
+```python
+$ python folder_create.py
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
 # Folder-Creation with python
--- folder_create.py
+++ folder_create.py
@@ -1,20 +1,29 @@
+#!/usr/bin/python
+
 import os
+'''
+    *os* and *os.path* modules include many functions to interact with the file system.
+'''
 import shutil
+
+
 class fun:
     def __init__(self,op,f_name,File_name):
         self.op=op
         self.f_name=f_name
         self.File_name=File_name
+        
     @classmethod
     def create(cls,f_name,File_name):
         if os.path.exists(f_name):
             print("folder Alredy Created.")
         else:
             os.mkdir(f_name)
-            with open(rf'{f_name}\{File_name}','w') as f:
+            # For linux separator is / and windows is \
+            with open(rf'{f_name}/{File_name}','w') as f:
                 f.write("Hello World.")
                 print("Folder and File Created")
-    
+
     @classmethod
     def delete(cls,f_name):
         if os.path.exists(f_name):
@@ -23,17 +32,32 @@ class fun:
         else:
             print("Folder Dosen't exist.")
 
+    @classmethod
+    def create_README_file(self, f_name):
+        '''
+        if os.path.exists(f_name):
+            print("Folder Alredy Created")
+        else:
+        '''
+        try:
+            os.chdir(f_name)
+            with open("README.md", "w") as f:
+                f.write("Helo, README file!")
+        except FileNotFoundError:
+            print("File not created!")
+        
 
 
-
-print("1.Create Folder.\n2.Delete Folder.\n3.Dircetory List.\n4.Change Dir.\n5.Exit.")
+print("1.Create Folder.\n2.Create README file.\n3.Delete Folder.\n4.Dircetory List.\n5.Change Dir.\n6.Exit.")
+print("Operating System Module : ", os.name)
+print("Current Working Directory: ", os.getcwd())
 
 while True:
     try:
         ch=int(input("Enter Choice: "))
     except ValueError:
             print("Enter Valid Choice: ")
-    
+
     else:
         break
 
@@ -41,6 +65,9 @@ while True:
 if ch==1:
     name,filename=input("Enter Folder && File Name: ").split(',')
     fun2=fun.create(name,filename)
+
+#elif ch==2:
+    fun.create_README_file(name)
 elif ch==2:
     name=input("Enter Folder name: ")
     fun2=fun.delete(name)
@@ -53,12 +80,4 @@ elif ch==4:
     print(os.listdir())
 else:
     exit
-    print("System Closed")       
-
-
-
-
-
-
-
-    
+    print("System Closed")
